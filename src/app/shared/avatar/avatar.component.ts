9247e94e8eb44e6f5e74d12b7972fae8baaabac9
Jiya Gupta <jiya.gupta@joshtechnologygroup.com>
2026-07-18 00:17:53 +0530

    [JG_A2_HEADER]: add fallback to avatar component

--- src/app/shared/avatar/avatar.component.ts
+++ src/app/shared/avatar/avatar.component.ts
@@ -1,12 +1,20 @@
-import { Input, Component } from '@angular/core';
+import { Input, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
 })
-export class AvatarComponent {
+export class AvatarComponent implements OnInit {
   @Input({ required: true }) src!: string;
   @Input() alt = 'Avatar';
   @Input() size: 'sm' | 'md' | 'lg' = 'md';
+  @Input() fallback = 'assets/avatar_01.png';
+  currentSrc!: string;
+  ngOnInit() {
+    this.currentSrc = this.src;
+  }
+  onError() {
+    this.currentSrc = this.fallback;
+  }
 }
