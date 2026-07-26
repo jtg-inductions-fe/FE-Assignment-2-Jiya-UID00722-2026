c03e941866c38887e76373937dbaba5a2c7d1674
Jiya Gupta <jiya.gupta@joshtechnologygroup.com>
2026-07-26 17:03:51 +0530

    [JG_A2_SIDEBAR]: show sidebar hamburger only at dashboard routes

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { BehaviorSubject, filter } from 'rxjs';
 import { AuthService } from '@core/services/auth.service';
 import { ASSETS } from '@core/constants/assets';
 import * as ButtonTypes from '@shared/button/button.types';
@@ -14,14 +16,32 @@ export class HeaderComponent {
   private authService = inject(AuthService);
   private sidebarService = inject(SidebarService);
   private responsiveService = inject(ResponsiveService);
+  private router = inject(Router);
 
   readonly ButtonTypes = ButtonTypes;
   imageUrl = ASSETS.IMAGES.LOGO;
+
+  private showSidebarToggleSubject = new BehaviorSubject<boolean>(
+    this.isDashboardRoute(),
+  );
 
   currentUser$ = this.authService.currentUser$;
 
   public currentSidebarState$ = this.sidebarService.currentSidebarState$;
   public isMobile$ = this.responsiveService.isMobile$;
+  showSidebarToggle$ = this.showSidebarToggleSubject.asObservable();
+
+  constructor() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.showSidebarToggleSubject.next(this.isDashboardRoute());
+      });
+  }
+
+  private isDashboardRoute(): boolean {
+    return this.router.url.startsWith('/dashboard');
+  }
 
   toggleSidebar(): void {
     this.sidebarService.toggleSidebar();
