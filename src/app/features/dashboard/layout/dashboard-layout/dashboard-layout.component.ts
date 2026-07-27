fbfee103af8015f5ee66ab85902afcfbf1d43184
Jiya Gupta <jiya.gupta@joshtechnologygroup.com>
2026-07-27 13:02:38 +0530

    [JG_A2_SIDEBAR]: use oninit

--- src/app/features/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/features/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ResponsiveService } from '@core/services/responsive.service';
 import { SidebarService } from '@core/services/sidebar.service';
 import { distinctUntilChanged } from 'rxjs';
@@ -9,14 +9,14 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   templateUrl: './dashboard-layout.component.html',
   styleUrls: ['./dashboard-layout.component.scss'],
 })
-export class DashboardLayoutComponent {
+export class DashboardLayoutComponent implements OnInit {
   private responsiveService = inject(ResponsiveService);
   private sidebarService = inject(SidebarService);
 
   public isMobile$ = this.responsiveService.isMobile$;
   public currentSidebarState$ = this.sidebarService.currentSidebarState$;
 
-  constructor() {
+  ngOnInit(): void {
     this.isMobile$
       .pipe(distinctUntilChanged(), takeUntilDestroyed())
       .subscribe(isMobile => {
